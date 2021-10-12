c163846d6ba212f1b8c1bed6ddd9615a142e74f7
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2021-10-12 13:34:38 -0700

    fix(helm): avoid unnecessary Helm builds
    Previously, Helm modules would get rebuilt ahead of deployment, even if
    the service version hadn't changed (thus leading deploy tasks to not
    redeploy the service, making the build step unnecessary).
    
    We address that here by making the Helm build handler a no-op, and
    instead running its logic inside the deployment handler.

--- core/src/plugins/kubernetes/helm/deployment.ts
+++ core/src/plugins/kubernetes/helm/deployment.ts
@@ -6,10 +6,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import tmp from "tmp-promise"
 import { waitForResources } from "../status/status"
 import { helm } from "./helm-cli"
 import { HelmModule } from "./config"
-import { getChartPath, getReleaseName, getChartResources, getValueArgs, getBaseModule } from "./common"
+import {
+  getChartPath,
+  getReleaseName,
+  getChartResources,
+  getValueArgs,
+  getBaseModule,
+  containsBuildSource,
+} from "./common"
 import { getReleaseStatus, HelmServiceStatus, getDeployedResources } from "./status"
 import { HotReloadableResource } from "../hot-reload/hot-reload"
 import { apply, deleteResources } from "../kubectl"
@@ -22,6 +30,12 @@ import { getServiceResource, getServiceResourceSpec } from "../util"
 import { getModuleNamespace, getModuleNamespaceStatus } from "../namespace"
 import { getHotReloadSpec, configureHotReload, getHotReloadContainerName } from "../hot-reload/helpers"
 import { configureDevMode, startDevModeSync } from "../dev-mode"
+import { move } from "fs-extra"
+import { basename, join } from "path"
+import { ConfigurationError } from "../../../exceptions"
+import { BuildModuleParams, BuildResult } from "../../../types/plugin/module/build"
+import { deline } from "../../../util/string"
+import { LogEntry } from "../../../logger/log-entry"
 
 export async function deployHelmService({
   ctx,
@@ -32,6 +46,7 @@ export async function deployHelmService({
   devMode,
   hotReload,
 }: DeployServiceParams<HelmModule>): Promise<HelmServiceStatus> {
+  await prepareHelmModule({ ctx, module, log })
   let hotReloadSpec: ContainerHotReloadSpec | null = null
   let serviceResourceSpec: ServiceResourceSpec | null = null
   let serviceResource: HotReloadableResource | null = null
@@ -178,4 +193,64 @@ export async function deleteService(params: DeleteServiceParams): Promise<HelmSe
   log.setSuccess("Service deleted")
 
   return { state: "missing", detail: { remoteResources: [] } }
+}
+
+/**
+ * Fetches the module's Helm chart and updates local Helm repos if necessary.
+ */
+export async function prepareHelmModule({ ctx, module, log }: BuildModuleParams<HelmModule>): Promise<BuildResult> {
+  const k8sCtx = <KubernetesPluginContext>ctx
+  const baseModule = getBaseModule(module)
+
+  if (!baseModule && !(await containsBuildSource(module))) {
+    if (!module.spec.chart) {
+      throw new ConfigurationError(
+        deline`Module '${module.name}' neither specifies a chart name, base module,
+        nor contains chart sources at \`chartPath\`.`,
+        { module }
+      )
+    }
+    log.debug("Fetching chart...")
+    try {
+      await pullChart(k8sCtx, log, module)
+    } catch {
+      // Update the local helm repos and retry
+      log.debug("Updating Helm repos...")
+      // The stable repo is no longer added by default
+      await helm({
+        ctx: k8sCtx,
+        log,
+        args: ["repo", "add", "stable", "https://charts.helm.sh/stable", "--force-update"],
+      })
+      await helm({ ctx: k8sCtx, log, args: ["repo", "update"] })
+      log.debug("Fetching chart (after updating)...")
+      await pullChart(k8sCtx, log, module)
+    }
+  }
+
+  return { fresh: true }
+}
+
+async function pullChart(ctx: KubernetesPluginContext, log: LogEntry, module: HelmModule) {
+  const chartPath = await getChartPath(module)
+  const chartDir = basename(chartPath)
+
+  const tmpDir = await tmp.dir({ unsafeCleanup: true })
+
+  try {
+    const args = ["pull", module.spec.chart!, "--untar", "--untardir", tmpDir.path]
+
+    if (module.spec.version) {
+      args.push("--version", module.spec.version)
+    }
+    if (module.spec.repo) {
+      args.push("--repo", module.spec.repo)
+    }
+
+    await helm({ ctx, log, args: [...args], cwd: tmpDir.path })
+
+    await move(join(tmpDir.path, chartDir), chartPath, { overwrite: true })
+  } finally {
+    await tmpDir.cleanup()
+  }
 }
