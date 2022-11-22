c5bfb7a29bce432f546cff50d68ca7ccf6d6fb69
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2022-11-22 10:05:23 +0100

    chore(examples): local mode config for frontend app

--- examples/local-mode/frontend-local/app.js
+++ examples/local-mode/frontend-local/app.js
@@ -5,7 +5,7 @@ const app = express();
 const backend1ServiceEndpoint = `http://backend-1/hello-backend-1`
 const backend2ServiceEndpoint = `http://backend-2/hello-backend-2`
 
-app.get('/hello-frontend', (req, res) => res.send('Hello from the frontend!'));
+app.get('/hello-frontend', (req, res) => res.send('Hello from the local frontend!'));
 
 app.get('/call-backend-1', (req, res) => {
   // Query the backend and return the response
